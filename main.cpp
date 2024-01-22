f8523aec61018dbab4c81c3af9c003a9daca9006
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2024-01-22 15:16:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,80 @@
 #include "iostream"
+#include "fstream"
+using  namespace std;
 
 
-using namespace std;
+// gcc main.cpp -lstdc++
 
-int Login(){}
+void ToDo(string username, string password){
+    cout << username << endl << password << endl;
+}
 
-int Register(){}
+int Login(){
+    string username;
+    string password;
+    // get Password and Username
+    cout << "Please enter your Username: " << endl;
+    cin >> username;
+    cout << "Please enter your Password" << endl;
+    cin >> password;
+    return 0;
+}
+
+
+
+int Register()
+{
+    string username;
+    string password;
+    // get Password and Username
+    cout << "Please enter your Username: " << endl;
+    cin >> username;
+    cout << "Please enter your Password" << endl;
+    cin >> password;
+
+
+    //Check for non viable Name and password
+    if (sizeof(username) < 2){
+        cout << "Username is not viable, Please enter a name with the length of 2-8 characters" << endl;
+        system("cls");
+        Register();
+    }
+
+    if (sizeof(password) < 5){
+        cout << "Password is not viable, Please enter a password with the length of 2-8 characters" << endl;
+        system("cls");
+        Register();
+    }
+    ifstream file;
+    file.open("infos.txt");
+
+    if (file){
+        string deleteFile;
+        cout << "There is a info File already existing? Do you want to DELETE it and create a new account? yes/no" << endl;
+        cin >> deleteFile;
+        if (deleteFile == "yes"){
+            remove("infos.h");
+            system("cls");
+        }else {system("cls"); main();}
+    }
+
+
+    if (!file){
+        ofstream filecreate("infos.txt");
+        filecreate << username << endl;
+        filecreate << password << endl;        
+    }
+
+
+    ToDo(username, password);
+
+    return 0;
+}
 
 // Check for Login Method
 int main(){
     string LoginMethod;
-    bool hasAccount;
-    string username;
-    string password;
+
     cout << "Welcome to ComplexiDo" << endl;
     cout << "Do you want to login or Register? login/register" << endl; 
     cin >> LoginMethod;
@@ -24,5 +86,5 @@ int main(){
     if (LoginMethod == "register"){
         Register();
     }
-
+    return 0;
 }
