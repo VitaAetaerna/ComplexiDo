f0f94cdbd9762efe07c0015eff749a7060e7e7e1
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2024-01-24 11:45:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,45 @@
 #include "iostream"
 #include "fstream"
-using  namespace std;
+using namespace std;
+string username;
+string password;
 
 // gcc main.cpp -lstdc++
 
-string ShowTasks(){
-    // READ FILE AND PRINT IT CLEAR CMD AND RETURN TO TODO LIST
+string ShowTasks()
+{
+    // READ FILE AND PRINT IT, CLEAR CMD AND RETURN TO TODO LIST
+    system("cls");
+    ifstream fileToPrint;
+    fileToPrint.open("tasks.txt");
+
+    string content;
+    if (fileToPrint){
+        fileToPrint >> content;
+        cout << content << endl << endl;
+    }
+    
+    return content;
+}
+
+string AddTask(){
+    string taskscontent;
+    ofstream fileToWrite("tasks.txt");
+    cout << "Enter Task: ";
+    cin >> taskscontent;
+    fileToWrite << taskscontent;
+
+    return taskscontent;
 }
 
 
-void ToDo(string username, string password){
+
+void ToDo(string username, string password)
+{
     ofstream TaskFile;
     TaskFile.open("tasks.txt");
-    if (!TaskFile){
+    if (!TaskFile)
+    {
         fstream TaskFileCreate("tasks.txt");
     }
     fstream TaskFileCreate("tasks.txt");
@@ -27,72 +54,111 @@ void ToDo(string username, string password){
     cout << "                                           ";
     // Check Choice of User
     cin >> choice;
-    switch (choice){
-        case 1:
-            cout << "Print all Tasks or none if none is existent" << endl;
-            ShowTasks();
-        case 2: 
-            cout << "Adding Tasks" << endl;
-        case 3:
-            cout << "Delete Tasks" << endl;
-        case 4:
-            cout << "Change Tasks" << endl;
-        case 5:
-            cout << "Change Tasks" << endl;
+    switch (choice)
+    {
+    case 1:
+        cout << "Print all Tasks or none if none is existent" << endl;
+        ShowTasks();
+    case 2:
+        cout << "Adding Tasks" << endl;
+    case 3:
+        cout << "Delete Tasks" << endl;
+    case 4:
+        cout << "Change Tasks" << endl;
+    case 5:
+        cout << "Change Tasks" << endl;
     }
-
 }
 
-int Login(){
-    string username;
-    string password;
+int Login()
+{
+    string savedUsername;
+    string savedPassword;
     // get Password and Username
     cout << "Please enter your Username: " << endl;
     cin >> username;
     cout << "Please enter your Password" << endl;
     cin >> password;
+
+    ifstream fileForLogin("infos.txt");
+
+
+    if (fileForLogin){
+
+        int currentline = 0;
+        while(getline(fileForLogin, savedUsername)){
+            if (currentline == 1){
+                break;
+            }
+            currentline++;
+        }
+        while(getline(fileForLogin, savedPassword)){
+            if (currentline == 2){
+                break;
+            }
+            currentline++;
+        }
+
+    }
+
+    if (username != savedUsername){
+        cout << "Try again, wrong Username!" << endl;
+        system("cls");
+        Login();
+    }
+    if (password != savedPassword){
+        cout << "Try again, wrong Password" << endl;
+        system("cls");
+        Login();
+    }
+    
+
     return 0;
 }
 
-
-
 int Register()
 {
-    string username;
-    string password;
+
     // get Password and Username
     cout << "Please enter your Username: " << endl;
     cin >> username;
     cout << "Please enter your Password" << endl;
     cin >> password;
 
-
-    //Check for non viable Name and password
-    if (sizeof(username) < 2 || sizeof(password) < 5){
+    // Check for non viable Name and password
+    if (sizeof(username) < 2 || sizeof(password) < 5)
+    {
         cout << "Username or Password is not viable, Please enter a name with the length of 2-8 characters and a Password of atleast 5 character" << endl;
         Register();
     }
-    //Open File in Ifstream
+    // Open File in Ifstream
     ifstream file;
     file.open("infos.txt");
-    //If File exists warn user
-    if (file){
+    // If File exists warn user
+    if (file)
+    {
         string deleteFile;
         cout << "There is a info File already existing? Do you want to DELETE it and create a new account? yes/no" << endl;
         cin >> deleteFile;
-        if (deleteFile == "yes"){
-            remove("infos.h");
+        if (deleteFile == "yes")
+        {
+            remove("infos.txt");
             system("cls");
-        }else {system("cls"); system("exit");}
+        }
+        else
+        {
+            system("cls");
+            system("exit");
+        }
     }
 
-    //Write To new File
-    if (!file){
+    // Write To new File
+    if (!file)
+    {
         ofstream filecreate("infos.txt");
         filecreate << username << endl;
-        filecreate << password << endl;        
+        filecreate << password << endl;
     }
-
 
     ToDo(username, password);
 
@@ -100,18 +166,21 @@ int Register()
 }
 
 // Check for Login Method
-int main(){
+int main()
+{
     string LoginMethod;
 
     cout << "Welcome to ComplexiDo" << endl;
-    cout << "Do you want to login or Register? login/register" << endl; 
+    cout << "Do you want to login or Register? login/register" << endl;
     cin >> LoginMethod;
 
     // Check if user wants to register or login
-    if (LoginMethod == "login"){
+    if (LoginMethod == "login")
+    {
         Login();
     }
-    if (LoginMethod == "register"){
+    if (LoginMethod == "register")
+    {
         Register();
     }
     return 0;
